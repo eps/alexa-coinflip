22424d151600ef67e1dcb28061c7973e6cde7565
Herman Chan <hermchan89@gmail.com>
2018-03-31 12:32:27 -0700

    update alexa skill

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const Alexa = require('alexa-sdk');
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
 //Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
-const APP_ID = '[APP_ID HERE]';
-
-const SKILL_NAME = 'Coin Flip';
-const HELP_MESSAGE = 'You can say flip a coin, or, you can say exit... What can I help you with?'
-const HELP_REPROMPT = 'What can I help you with?';
+const APP_ID = 'amzn1.ask.skill.b8e52f04-0ecc-44a0-bbe5-b1bd452bce24';
+const SKILL_NAME = 'Coin Flipper';
+const HELP_MESSAGE = 'This skill flips a coin for you. Would you like to flip?'
 const STOP_MESSAGE = 'Goodbye!';
 
 //=========================================================================================================================================
@@ -21,36 +19,37 @@ const STOP_MESSAGE = 'Goodbye!';
 //=========================================================================================================================================
 //Editing anything below this line might break your skill.
 //=========================================================================================================================================
-
-const handlers = {
+var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetCoinFlip');
+        this.emit(':ask', 'Welcome to Coin Flipper Skill! Try saying toss me.', 'Say toss me!');
     },
-    'GetCoinFlip': function () {
-        const result = Math.floor(Math.random() * 2 == 0) ? 'heads' : 'tails';
-        const speechOutput = 'the coin landed on:' + result;
-        this.response.cardRenderer(SKILL_NAME);
-        this.response.speak(speechOutput);
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, result)
+    'CoinFlipIntent': function () {
+      const result = Math.random();
+      let flipResult = ''
+      if (result > .5) {
+        flipResult = 'heads.';
+      } else {
+        flipResult = 'tails.';
+      }
+        let speechOutput = 'the coin landed on ' + flipResult;
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, flipResult)
     },
     'AMAZON.HelpIntent': function () {
-        const speechOutput = HELP_MESSAGE;
-        const reprompt = HELP_REPROMPT;
-
-        this.response.speak(speechOutput).listen(reprompt);
-        this.emit(':responseReady');
+        var speechOutput = HELP_MESSAGE;
+        this.emit(':ask', speechOutput);
     },
     'AMAZON.CancelIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
-    },
-    'AMAZON.YesIntent': function () {
-        this.emit('GetCoinFlip');
+        this.emit(':tell', STOP_MESSAGE);
     },
     'AMAZON.StopIntent': function () {
-        this.response.speak(STOP_MESSAGE);
-        this.emit(':responseReady');
+        this.emit(':tell', STOP_MESSAGE);
     },
+     'AMAZON.YesIntent': function () {
+        this.emit('CoinFlipIntent');
+    },
+     'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
 };
 
 exports.handler = function (event, context, callback) {
